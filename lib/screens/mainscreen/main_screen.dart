adc2ccaacb474fb40592568ea54826710fcd063b
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-03-24 14:30:51 +0330

    Main Screen Navigation

--- lib/screens/mainscreen/main_screen.dart
+++ lib/screens/mainscreen/main_screen.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:watch_store/gen/assets.gen.dart';
+import 'package:watch_store/screens/mainscreen/cart_screen.dart';
+import 'package:watch_store/screens/mainscreen/home_screen.dart';
+import 'package:watch_store/screens/mainscreen/profile_screen.dart';
 import 'package:watch_store/widget/btm_nav_item.dart';
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+class BtmNavScreenIndex {
+  BtmNavScreenIndex._();
+
+  static const home = 0;
+  static const cart = 1;
+  static const profile = 2;
+}
+
+class MainScreen extends StatefulWidget {
+  MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int selectedIndex = BtmNavScreenIndex.home;
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +36,13 @@ class MainScreen extends StatelessWidget {
               right: 0,
               left: 0,
               bottom: btmNavHeight,
-              child: Container(
-                color: Colors.red,
+              child: IndexedStack(
+                index: selectedIndex,
+                children: const [
+                  HomeScreen(),
+                  CartScreen(),
+                  ProfileScreen()
+                ],
               )),
           Positioned(
               bottom: 0,
@@ -32,14 +55,32 @@ class MainScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    BtmNavItem(iconPath: Assets.svg.user.path, text: "پروفایل", isActive: false, onTap: (){}),
-                    BtmNavItem(iconPath: Assets.svg.cart.path, text: "سبد خرید", isActive: false, onTap: (){}),
-                    BtmNavItem(iconPath: Assets.svg.home.path, text: "خانه", isActive: true, onTap: (){}),
+                    BtmNavItem(
+                        iconPath: Assets.svg.user.path,
+                        text: "پروفایل",
+                        isActive: selectedIndex==BtmNavScreenIndex.profile,
+                        onTap: ()=>btmNavOnPressed(BtmNavScreenIndex.profile)),
+                    BtmNavItem(
+                        iconPath: Assets.svg.cart.path,
+                        text: "سبد خرید",
+                        isActive: selectedIndex==BtmNavScreenIndex.cart,
+                        onTap: ()=>btmNavOnPressed(BtmNavScreenIndex.cart)),
+                    BtmNavItem(
+                        iconPath: Assets.svg.home.path,
+                        text: "خانه",
+                        isActive: selectedIndex==BtmNavScreenIndex.home,
+                        onTap: ()=>btmNavOnPressed(BtmNavScreenIndex.home)),
                   ],
                 ),
               ))
         ],
       ),
     ));
+  }
+
+  btmNavOnPressed(int index){
+    setState(() {
+      selectedIndex=index;
+    });
   }
 }
