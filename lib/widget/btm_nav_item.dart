8b498d6dae7c166f45e54da9e94d37d6f6186481
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-03-24 13:32:45 +0330

    BtmNavItem

---
+++ lib/widget/btm_nav_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:watch_store/component/extension.dart';
+import 'package:watch_store/res/colors.dart';
+
+import '../component/text_style.dart';
+
+class BtmNavItem extends StatelessWidget {
+  final String iconPath;
+  final String text;
+  final bool isActive;
+  final void Function() onTap;
+
+  const BtmNavItem(
+      {super.key,
+      required this.iconPath,
+      required this.text,
+      required this.isActive,
+      required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              iconPath,
+              colorFilter: ColorFilter.mode(
+                  isActive
+                      ? AppColors.btmNavActiveItem
+                      : AppColors.btmNavInActiveItem,
+                  BlendMode.srcIn),
+            ),
+            8.height,
+            Text(
+              text,
+              style: isActive?AppTextStyles.btmNavActiveItem:AppTextStyles.btmNavInActiveItem,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
