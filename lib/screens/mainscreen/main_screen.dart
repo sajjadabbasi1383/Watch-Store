fc6bf07bc38da8080b732739f95120754ca7bd4a
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-03-26 14:10:33 +0330

    WillPopScope

--- lib/screens/mainscreen/main_screen.dart
+++ lib/screens/mainscreen/main_screen.dart
@@ -32,77 +32,85 @@ class _MainScreenState extends State<MainScreen> {
     BtmNavScreenIndex.profile:_profilekey,
   };
 
-
+  Future<bool> _onWillPop()async{
+    if(map[selectedIndex]!.currentState!.canPop()){
+      map[selectedIndex]!.currentState!.pop();
+    }
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.sizeOf(context);
     double btmNavHeight = size.height * .1;
-    return SafeArea(
-        child: Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-              top: 0,
-              right: 0,
-              left: 0,
-              bottom: btmNavHeight,
-              child: IndexedStack(
-                index: selectedIndex,
-                children: [
-                  Navigator(
-                    key: _homekey,
-                    onGenerateRoute: (settings) => MaterialPageRoute(
-                      builder: (context) => const HomeScreen(),
-                    ),
-                  ),
-                  Navigator(
-                    key: _cartkey,
-                    onGenerateRoute: (settings) => MaterialPageRoute(
-                      builder: (context) => const CartScreen(),
-                    ),
-                  ),
-                  Navigator(
-                    key: _profilekey,
-                    onGenerateRoute: (settings) => MaterialPageRoute(
-                      builder: (context) => const ProfileScreen(),
-                    ),
-                  ),
-                ],
-              )),
-          Positioned(
-              bottom: 0,
-              right: 0,
-              left: 0,
-              child: Container(
-                height: btmNavHeight,
-                color: Colors.white,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: SafeArea(
+          child: Scaffold(
+        body: Stack(
+          children: [
+            Positioned(
+                top: 0,
+                right: 0,
+                left: 0,
+                bottom: btmNavHeight,
+                child: IndexedStack(
+                  index: selectedIndex,
                   children: [
-                    BtmNavItem(
-                        iconPath: Assets.svg.user.path,
-                        text: "پروفایل",
-                        isActive: selectedIndex == BtmNavScreenIndex.profile,
-                        onTap: () =>
-                            btmNavOnPressed(BtmNavScreenIndex.profile)),
-                    BtmNavItem(
-                        iconPath: Assets.svg.cart.path,
-                        text: "سبد خرید",
-                        isActive: selectedIndex == BtmNavScreenIndex.cart,
-                        onTap: () => btmNavOnPressed(BtmNavScreenIndex.cart)),
-                    BtmNavItem(
-                        iconPath: Assets.svg.home.path,
-                        text: "خانه",
-                        isActive: selectedIndex == BtmNavScreenIndex.home,
-                        onTap: () => btmNavOnPressed(BtmNavScreenIndex.home)),
+                    Navigator(
+                      key: _homekey,
+                      onGenerateRoute: (settings) => MaterialPageRoute(
+                        builder: (context) => const HomeScreen(),
+                      ),
+                    ),
+                    Navigator(
+                      key: _cartkey,
+                      onGenerateRoute: (settings) => MaterialPageRoute(
+                        builder: (context) => const CartScreen(),
+                      ),
+                    ),
+                    Navigator(
+                      key: _profilekey,
+                      onGenerateRoute: (settings) => MaterialPageRoute(
+                        builder: (context) => const ProfileScreen(),
+                      ),
+                    ),
                   ],
-                ),
-              ))
-        ],
-      ),
-    ));
+                )),
+            Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: Container(
+                  height: btmNavHeight,
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      BtmNavItem(
+                          iconPath: Assets.svg.user.path,
+                          text: "پروفایل",
+                          isActive: selectedIndex == BtmNavScreenIndex.profile,
+                          onTap: () =>
+                              btmNavOnPressed(BtmNavScreenIndex.profile)),
+                      BtmNavItem(
+                          iconPath: Assets.svg.cart.path,
+                          text: "سبد خرید",
+                          isActive: selectedIndex == BtmNavScreenIndex.cart,
+                          onTap: () => btmNavOnPressed(BtmNavScreenIndex.cart)),
+                      BtmNavItem(
+                          iconPath: Assets.svg.home.path,
+                          text: "خانه",
+                          isActive: selectedIndex == BtmNavScreenIndex.home,
+                          onTap: () => btmNavOnPressed(BtmNavScreenIndex.home)),
+                    ],
+                  ),
+                ))
+          ],
+        ),
+      )),
+    );
   }
 
   btmNavOnPressed(int index) {
