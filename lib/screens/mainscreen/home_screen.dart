419565cef23a7c3c736a957fd4e5b0c5377f4586
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-03-28 16:23:18 +0330

    Category List

--- lib/screens/mainscreen/home_screen.dart
+++ lib/screens/mainscreen/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:watch_store/gen/assets.gen.dart';
+import 'package:watch_store/res/colors.dart';
+import 'package:watch_store/res/strings.dart';
+import 'package:watch_store/widget/cat_widget.dart';
 
 import '../../widget/app_slider.dart';
 import '../../widget/search_btn.dart';
@@ -15,11 +19,39 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             SearchBtn(onTap: () {},),
-            AppSlider(),
+            const AppSlider(),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CatWidget(
+                    colors: AppColors.catDesktopColors,
+                    iconPath: Assets.svg.desktop.path,
+                    title: AppStrings.desktop,
+                    onTap: (){}),
+                CatWidget(
+                    colors: AppColors.catDigitalColors,
+                    iconPath: Assets.svg.digital.path,
+                    title: AppStrings.digital,
+                    onTap: (){}),
+                CatWidget(
+                    colors: AppColors.catSmartColors,
+                    iconPath: Assets.svg.smart.path,
+                    title: AppStrings.smart,
+                    onTap: (){}),
+                CatWidget(
+                    colors: AppColors.catClassicColors,
+                    iconPath: Assets.svg.clasic.path,
+                    title: AppStrings.classic,
+                    onTap: (){}),
+              ],
+            )
+
           ],
         ),
       )
     ));
   }
 }
+
 
---
+++ lib/widget/cat_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../component/text_style.dart';
+import '../res/dimens.dart';
+
+class CatWidget extends StatelessWidget {
+  const CatWidget({
+    super.key,
+    required this.colors,
+    required this.iconPath,
+    required this.title,
+    required this.onTap
+  });
+
+  final void Function() onTap;
+  final String title;
+  final String iconPath;
+  final List<Color> colors;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size=MediaQuery.sizeOf(context);
+    return GestureDetector(
+    onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(AppDimens.small),
+            width: size.height*.095,
+            height: size.height*.095,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: colors
+                ),
+                borderRadius: BorderRadius.circular(AppDimens.large)
+            ),
+            child: SvgPicture.asset(iconPath),
+          ),
+          Text(title,style: AppTextStyles.catTitle,)
+        ],
+      ),
+    );
+  }
+}
