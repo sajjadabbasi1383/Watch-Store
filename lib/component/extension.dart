e37306c57561dcd1c860848f224be4632ad8f95b
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-03-28 21:30:16 +0330

    separateWithComma Extension

--- lib/component/extension.dart
+++ lib/component/extension.dart
@@ -1,8 +1,16 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 extension SizedBoxExtension on num{
   SizedBox get height =>SizedBox(height: toDouble(),);
   SizedBox get width =>SizedBox(width: toDouble(),);
+}
+
+extension IntExtension on int{
+  String get separateWithComma{
+    final numberFormat=NumberFormat.decimalPattern();
+    return numberFormat.format(this);
+  }
 }
