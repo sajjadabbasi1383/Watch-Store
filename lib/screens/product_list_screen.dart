ed1a4573057c77461011c678149f4cfda7911a0d
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-04-02 13:46:29 +0330

    Product List Screen AppBar

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -5,12 +5,57 @@ class ProductListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        color: Colors.white,
-        child: Center(child: Text("لیست محصولات"),),
+    Size size = MediaQuery.sizeOf(context);
+    return SafeArea(
+        child: Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size(size.width, size.height * .07),
+        child: Container(
+          height: size.height * .07,
+          decoration: const BoxDecoration(
+              color: AppColors.appbar,
+              boxShadow: [
+                BoxShadow(
+                    color: AppColors.shadow,
+                    offset: Offset(0, 2),
+                    blurRadius: 3)
+              ],
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(AppDimens.medium),
+                bottomRight: Radius.circular(AppDimens.medium),
+              )),
+          child: Padding(
+            padding: const EdgeInsets.only(
+                left: AppDimens.medium, right: AppDimens.medium),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const CartBadge(count: 2),
+                Row(
+                  children: [
+                    const Text(
+                      "پرفروش ترین ها",
+                      style: AppTextStyles.avatarText,
+                    ),
+                    AppDimens.small.width,
+                    SvgPicture.asset(
+                      Assets.svg.sort.path,
+                      height: 27,
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: SvgPicture.asset(Assets.svg.close.path),
+                  splashRadius: 0.1,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text("لیست محصولات"),
       ),
     ));
   }
