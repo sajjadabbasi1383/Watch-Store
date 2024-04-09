a5629a97d82bbd2115eb58af49f8f852a748b731
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-04-09 17:03:37 +0330

    Avatar Profile Screen

--- lib/screens/mainscreen/profile_screen.dart
+++ lib/screens/mainscreen/profile_screen.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:watch_store/component/extension.dart';
+import 'package:watch_store/gen/assets.gen.dart';
+import 'package:watch_store/res/dimens.dart';
+import 'package:watch_store/widget/surface_container.dart';
 
+import '../../component/text_style.dart';
+import '../../res/strings.dart';
+import '../../widget/app_bar.dart';
 import '../product_single_screen.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -7,21 +15,68 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        color: Colors.yellow,
-        child: Center(
-          child: ElevatedButton(
-              onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ProductSingleScreen(),
-                  )),
-              child: const Text("مشاهده همه")),
+    return SafeArea(
+      child: Scaffold(
+        appBar: const CustomAppBar(
+            child: Align(
+          alignment: Alignment.centerRight,
+          child: Text(
+            AppStrings.profile,
+            style: AppTextStyles.appBarText,
+          ),
+        )),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: AppDimens.large),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  AppDimens.large.height,
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(300),
+                    child: SvgPicture.asset(
+                      Assets.svg.avatar.path,
+                      height: 80,
+                    ),
+                  ),
+                  AppDimens.small.height,
+                  const Text(
+                    "سجاد عباسی",
+                    style: AppTextStyles.avatarText,
+                  ),
+                  AppDimens.large.height,
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      AppStrings.activeAddress,
+                      style: AppTextStyles.productCaption,
+                    ),
+                  ),
+                  Align(
+                      alignment: Alignment.centerRight,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SvgPicture.asset(Assets.svg.leftArrow.path),
+                          const Text(
+                            AppStrings.fakeAddress,
+                            textDirection: TextDirection.rtl,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.right,
+                            style: AppTextStyles.selectedTab,
+                          ),
+                        ],
+                      )),
+                  const Divider(),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
-    ));
+    );
   }
 }
