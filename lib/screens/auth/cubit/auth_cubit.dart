dfbaf1fcd59b7599337039e231b5d0e8a9af1791
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-04-14 17:41:06 +0330

    send Sms Function

--- lib/screens/auth/cubit/auth_cubit.dart
+++ lib/screens/auth/cubit/auth_cubit.dart
@@ -1,8 +1,30 @@
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+import 'package:watch_store/data/src/api_constant.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
+
+  Dio _dio = Dio();
+
+  sendSms(String mobile) async {
+    emit(LoadingState());
+    try {
+      await _dio
+          .post(ApiConstant.sendSms, data: {"mobile": mobile}).then((value) {
+        debugPrint(value.toString());
+        if (value.statusCode == 201) {
+          emit(SentState());
+        } else {
+          emit(ErrorState());
+        }
+      });
+    } catch (e) {
+      emit(ErrorState());
+    }
+  }
 }
