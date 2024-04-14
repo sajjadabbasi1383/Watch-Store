d77ef18f808346cf069265b17c8e1b102a42a8da
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-04-14 17:40:49 +0330

    ApiConstant

---
+++ lib/data/src/api_constant.dart
@@ -0,0 +1,7 @@
+class ApiConstant{
+  ApiConstant._();
+  static const baseUrl="https://watchstore.sasansafari.com/public";
+  static const sendSms="$baseUrl/api/v1/send_sms";
+  static const checkSmsCode="$baseUrl/api/v1/check_sms_code";
+  static const register="$baseUrl/api/v1/register";
+}
