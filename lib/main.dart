5ffc78e51122d21cf3aeb2064f3582f9bd611b61
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-04-14 20:15:08 +0330

    BlocProvider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:watch_store/component/theme.dart';
 import 'package:watch_store/route_manager/routes.dart';
 import 'package:watch_store/route_manager/screen_names.dart';
+import 'package:watch_store/screens/auth/cubit/auth_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,14 +11,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: lightTheme(),
-       //home: SendOtpScreen(),
-      initialRoute: ScreenNames.root,
-      routes: routes,
+    return BlocProvider<AuthCubit>(
+      create: (context) => AuthCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: lightTheme(),
+        //home: SendOtpScreen(),
+        initialRoute: ScreenNames.root,
+        routes: routes,
+      ),
     );
   }
 }
