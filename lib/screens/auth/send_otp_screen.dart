a3573190cccb095c47ea797f5512bdfc5e3b955d
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-04-15 17:55:41 +0330

    BlocConsumer (listener) sendOtpScreen

--- lib/screens/auth/send_otp_screen.dart
+++ lib/screens/auth/send_otp_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:watch_store/component/extension.dart';
 import 'package:watch_store/gen/assets.gen.dart';
+import 'package:watch_store/res/colors.dart';
 import 'package:watch_store/res/dimens.dart';
 import 'package:watch_store/res/strings.dart';
 import 'package:watch_store/route_manager/screen_names.dart';
+import 'package:watch_store/screens/auth/cubit/auth_cubit.dart';
 import 'package:watch_store/widget/app_text_field.dart';
 import 'package:watch_store/widget/main_button.dart';
 
@@ -16,26 +20,41 @@ class SendOtpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(Assets.png.mainLogo.path),
-            AppDimens.large.height,
-            AppTextField(
-                lable: AppStrings.enterYourNumber,
-                hint: AppStrings.hintPhoneNumber,
-                controller: _controller,
-                align: TextAlign.center),
-            MainButton(
-              text: AppStrings.sendOtpCode,
-              onPressed: () => Navigator.pushNamed(context, ScreenNames.verifyCodeScreen),
-            )
-          ],
-        ),
-      ),
-    ));
+          body: SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(Assets.png.mainLogo.path),
+                AppDimens.large.height,
+                AppTextField(
+                    lable: AppStrings.enterYourNumber,
+                    hint: AppStrings.hintPhoneNumber,
+                    inputType: TextInputType.phone,
+                    controller: _controller,
+                    align: TextAlign.center),
+
+                BlocConsumer<AuthCubit, AuthState>(
+                  listener: (context, state) {
+                    if(state is SentState){
+                      Navigator.pushNamed(context, ScreenNames.verifyCodeScreen,arguments: state.mobile);
+                    }else if(state is ErrorState){
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                              content: Text("خطا در ارسال اطلاعات به سرور"),
+                        )
+                      );
+                    }
+                  },
+                  builder: (context, state) {
+
+                  },
+                )
+
+              ],
+            ),
+          ),
+        ));
   }
 }
