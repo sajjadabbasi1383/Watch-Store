012925a86e251fb23f9bd503ca9536e515830a3d
SajjadAbbasi <sajjadabbas0083@gmail.com>
2024-04-27 20:52:17 +0330

    validate textField

--- lib/screens/auth/send_otp_screen.dart
+++ lib/screens/auth/send_otp_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:watch_store/component/extension.dart';
@@ -15,9 +16,12 @@ import '../../component/text_style.dart';
 import '../../widget/snack_bar.dart';
 
 class SendOtpScreen extends StatelessWidget {
-  SendOtpScreen({super.key});
+  SendOtpScreen({
+    super.key,
+  });
 
   final TextEditingController _controller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -25,57 +29,65 @@ class SendOtpScreen extends StatelessWidget {
       child: Scaffold(
         body: SizedBox(
           width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(Assets.png.mainLogo.path),
-              AppDimens.large.height,
-              AppTextField(
-                lable: AppStrings.enterYourNumber,
-                hint: AppStrings.hintPhoneNumber,
-                inputType: TextInputType.phone,
-                controller: _controller,
-                align: TextAlign.center,
-                errorText: 'لطفا شماره تلفن را وارد کنید',
-              ),
-              BlocConsumer<AuthCubit, AuthState>(
-                listener: (context, state) {
-                  if (state is SentState) {
-                    Navigator.pushNamed(context, ScreenNames.verifyCodeScreen,
-                        arguments: state.mobile);
-                    showCustomSnackBar(
-                        context,
-                        "کد فعالسازی: ${state.code.toString()}",
-                        80,
-                        "successes");
-                  } else if (state is ErrorState) {
-                    showCustomSnackBar(
-                        context, "خطا در اتصال به سرور", 5, "error");
-                  }
-                },
-                builder: (context, state) {
-                  return MainButton(
-                    onPressed: state is LoadingState
-                        ? null
-                        : () {
-                            BlocProvider.of<AuthCubit>(context)
-                                .sendSms(_controller.text);
-
-                          },
-                    child: state is LoadingState
-                        ? LoadingAnimationWidget.prograssiveDots(
-                            color: Colors.white,
-                            size: 38,
-                          )
-                        : const Text(
-                            AppStrings.sendOtpCode,
-                            style: AppTextStyles.mainButton,
-                          ),
-                  );
-                },
-              ),
-            ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(Assets.png.mainLogo.path),
+                AppDimens.large.height,
+                AppTextField(
+                  lable: AppStrings.enterYourNumber,
+                  hint: AppStrings.hintPhoneNumber,
+                  inputType: TextInputType.phone,
+                  controller: _controller,
+                  align: TextAlign.center,
+                  errorText: 'لطفا شماره تلفن را وارد کنید',
+                  inputFormatter: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    LengthLimitingTextInputFormatter(11),
+                  ],
+                ),
+                BlocConsumer<AuthCubit, AuthState>(
+                  listener: (context, state) {
+                    if (state is SentState) {
+                      Navigator.pushNamed(context, ScreenNames.verifyCodeScreen,
+                          arguments: state.mobile);
+                      showCustomSnackBar(
+                          context,
+                          "کد فعالسازی: ${state.code.toString()}",
+                          80,
+                          "successes");
+                    } else if (state is ErrorState) {
+                      showCustomSnackBar(
+                          context, "خطا در اتصال به سرور", 5, "error");
+                    }
+                  },
+                  builder: (context, state) {
+                    return MainButton(
+                      onPressed: state is LoadingState
+                          ? null
+                          : () {
+                              _formKey.currentState!.validate()
+                                  ? BlocProvider.of<AuthCubit>(context)
+                                      .sendSms(_controller.text)
+                                  : null;
+                            },
+                      child: state is LoadingState
+                          ? LoadingAnimationWidget.prograssiveDots(
+                              color: Colors.white,
+                              size: 38,
+                            )
+                          : const Text(
+                              AppStrings.sendOtpCode,
+                              style: AppTextStyles.mainButton,
+                            ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
