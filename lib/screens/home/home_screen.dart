d103daad2615280582adbe3f25c08529cf8af083
SajjadAbbasi <sajjadabbas0083@gmail.com>
2024-04-28 16:30:24 +0330

    implement home items with API

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:watch_store/component/extension.dart';
+import 'package:watch_store/data/repo/home_repo.dart';
 import 'package:watch_store/gen/assets.gen.dart';
 import 'package:watch_store/res/colors.dart';
 import 'package:watch_store/res/dimens.dart';
@@ -11,147 +14,211 @@ import '../../widget/app_slider.dart';
 import '../../widget/product_item.dart';
 import '../../widget/search_btn.dart';
 import '../../widget/vertical_text.dart';
+import 'bloc/home_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return BlocProvider(
+      create: (context) {
+        final homeBloc = HomeBloc(homeRepository);
+        homeBloc.add(HomeInit());
+        return homeBloc;
+      },
+      child: SafeArea(
         child: Scaffold(
-            body: SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      child: Column(
-        children: [
-          SearchBtn(
-            onTap: () {},
-          ),
-          const AppSlider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CatWidget(
-                  colors: AppColors.catDesktopColors,
-                  iconPath: Assets.svg.desktop.path,
-                  title: AppStrings.desktop,
-                  onTap: () {}),
-              CatWidget(
-                  colors: AppColors.catDigitalColors,
-                  iconPath: Assets.svg.digital.path,
-                  title: AppStrings.digital,
-                  onTap: () {}),
-              CatWidget(
-                  colors: AppColors.catSmartColors,
-                  iconPath: Assets.svg.smart.path,
-                  title: AppStrings.smart,
-                  onTap: () {}),
-              CatWidget(
-                  colors: AppColors.catClassicColors,
-                  iconPath: Assets.svg.clasic.path,
-                  title: AppStrings.classic,
-                  onTap: () {}),
-            ],
-          ),
-          AppDimens.small.height,
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
+          body: SingleChildScrollView(
             physics: const BouncingScrollPhysics(),
-            reverse: true,
-            child: Row(
-              children: [
-                SizedBox(
-                  height: 300,
-                  child: ListView.builder(
-                    physics: const ClampingScrollPhysics(),
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 8,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) => ProductItem(
-                        productName: "ساعت مردانه",
-                        price: "${69000.separateWithComma} تومان",
-                        discount: 20,
-                        oldPrice: 122000.separateWithComma,
-                        timer: "09:26:38"),
-                  ),
-                ),
-                const VerticalText(
-                  title: "شگفت انگیز",
-                  textStyle: AppTextStyles.amazingProducts,
-                )
-              ],
-            ),
-          ),
+            child: BlocBuilder<HomeBloc, HomeState>(
+              builder: (context, state) {
+                if (state is HomeLoading) {
+                  return SizedBox(
+                    width: double.infinity,
+                    height: MediaQuery.sizeOf(context).height,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        LoadingAnimationWidget.staggeredDotsWave(
+                          color: AppColors.loadingColor,
+                          size: 40,
+                        ),
+                        AppDimens.small.height,
+                        const Text(
+                          "در حال تکمیل اطلاعات...",
+                          style: AppTextStyles.loadingText,
+                          textDirection: TextDirection.rtl,
+                        )
+                      ],
+                    ),
+                  );
+                } else if (state is HomeLoaded) {
+                  return Column(
+                    children: [
+                      SearchBtn(
+                        onTap: () {},
+                      ),
+                      AppSlider(
+                        imgList: state.home.sliders,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CatWidget(
+                              colors: AppColors.catDesktopColors,
+                              iconPath: Assets.svg.desktop.path,
+                              title: AppStrings.desktop,
+                              onTap: () {}),
+                          CatWidget(
+                              colors: AppColors.catDigitalColors,
+                              iconPath: Assets.svg.digital.path,
+                              title: AppStrings.digital,
+                              onTap: () {}),
+                          CatWidget(
+                              colors: AppColors.catSmartColors,
+                              iconPath: Assets.svg.smart.path,
+                              title: AppStrings.smart,
+                              onTap: () {}),
+                          CatWidget(
+                              colors: AppColors.catClassicColors,
+                              iconPath: Assets.svg.clasic.path,
+                              title: AppStrings.classic,
+                              onTap: () {}),
+                        ],
+                      ),
+                      AppDimens.small.height,
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        physics: const BouncingScrollPhysics(),
+                        reverse: true,
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              height: 320,
+                              child: ListView.builder(
+                                physics: const ClampingScrollPhysics(),
+                                scrollDirection: Axis.horizontal,
+                                itemCount: state.home.amazingProducts.length,
+                                shrinkWrap: true,
+                                itemBuilder: (context, index) => ProductItem(
+                                    image:
+                                        state.home.amazingProducts[index].image,
+                                    productName:
+                                        state.home.amazingProducts[index].title,
+                                    price:
+                                        "${state.home.amazingProducts[index].price.separateWithComma} تومان",
+                                    discount: state
+                                        .home.amazingProducts[index].discount,
+                                    oldPrice: state.home.amazingProducts[index]
+                                        .discountPrice.separateWithComma,
+                                    timer: "09:26:38"),
+                              ),
+                            ),
+                            const VerticalText(
+                              title: "شگفت انگیز",
+                              textStyle: AppTextStyles.amazingProducts,
+                            )
+                          ],
+                        ),
+                      ),
+                      AppDimens.small.height,
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        physics: const BouncingScrollPhysics(),
+                        reverse: true,
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              height: 320,
+                              child: ListView.builder(
+                                physics: const ClampingScrollPhysics(),
+                                scrollDirection: Axis.horizontal,
+                                itemCount: state.home.mostSellerProducts.length,
+                                shrinkWrap: true,
+                                itemBuilder: (context, index) => ProductItem(
+                                    image: state
+                                        .home.mostSellerProducts[index].image,
+                                    productName: state
+                                        .home.mostSellerProducts[index].title,
+                                    price:
+                                        "${state.home.mostSellerProducts[index].price.separateWithComma} تومان",
+                                    discount: state.home
+                                        .mostSellerProducts[index].discount,
+                                    oldPrice: state
+                                        .home
+                                        .mostSellerProducts[index]
+                                        .discountPrice
+                                        .separateWithComma,
+                                    timer: "09:26:38"),
+                              ),
+                            ),
+                            const VerticalText(
+                              title: "پرفروش ها",
+                              textStyle: AppTextStyles.bestSellersProducts,
+                            )
+                          ],
+                        ),
+                      ),
+                      AppDimens.small.height,
+                      Container(
+                        width: double.infinity,
+                        height: 180,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Image.asset(Assets.png.banner.path),
+                      ),
 
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            physics: const BouncingScrollPhysics(),
-            reverse: true,
-            child: Row(
-              children: [
-                SizedBox(
-                  height: 300,
-                  child: ListView.builder(
-                    physics: const ClampingScrollPhysics(),
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 8,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) => ProductItem(
-                        productName: "ساعت مردانه",
-                        price: "${69000.separateWithComma} تومان",
-                        discount: 20,
-                        oldPrice: 122000.separateWithComma,
-                        timer: "09:26:38"),
-                  ),
-                ),
-                const VerticalText(
-                  title: "پرفروش ها",
-                  textStyle: AppTextStyles.bestSellersProducts,
-                )
-              ],
+                      AppDimens.small.height,
+                      //AppDimens.small.height,
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        physics: const BouncingScrollPhysics(),
+                        reverse: true,
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              height: 320,
+                              child: ListView.builder(
+                                physics: const ClampingScrollPhysics(),
+                                scrollDirection: Axis.horizontal,
+                                itemCount: state.home.newestProducts.length,
+                                shrinkWrap: true,
+                                itemBuilder: (context, index) => ProductItem(
+                                    image:
+                                        state.home.newestProducts[index].image,
+                                    productName:
+                                        state.home.newestProducts[index].title,
+                                    price:
+                                        "${state.home.newestProducts[index].price.separateWithComma} تومان",
+                                    discount: state
+                                        .home.newestProducts[index].discount,
+                                    oldPrice: state.home.newestProducts[index]
+                                        .discountPrice.separateWithComma,
+                                    timer: "09:26:38"),
+                              ),
+                            ),
+                            const VerticalText(
+                              title: "جدیدترین ها",
+                              textStyle: AppTextStyles.newestProducts,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                } else if (state is HomeError) {
+                  return const Text("error");
+                } else {
+                  throw Exception("Invalid Home State");
+                }
+              },
             ),
           ),
-          AppDimens.small.height,
-          Container(
-            width: double.infinity,
-            height: 180,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Image.asset(Assets.png.banner.path),
-          ),
-
-          AppDimens.small.height,
-          //AppDimens.small.height,
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            physics: const BouncingScrollPhysics(),
-            reverse: true,
-            child: Row(
-              children: [
-                SizedBox(
-                  height: 300,
-                  child: ListView.builder(
-                    physics: const ClampingScrollPhysics(),
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 8,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) => ProductItem(
-                        productName: "ساعت مردانه",
-                        price: "${69000.separateWithComma} تومان",
-                        discount: 20,
-                        oldPrice: 122000.separateWithComma,
-                        timer: "09:26:38"),
-                  ),
-                ),
-                const VerticalText(
-                  title: "جدیدترین ها",
-                  textStyle: AppTextStyles.newestProducts,
-                )
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
-    )));
+    );
   }
 }
