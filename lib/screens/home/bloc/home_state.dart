4cda9fb6eee00642e99de0de076d27af3b63b64d
SajjadAbbasi <sajjadabbas0083@gmail.com>
2024-04-28 16:27:10 +0330

    home event and home state

--- lib/screens/home/bloc/home_state.dart
+++ lib/screens/home/bloc/home_state.dart
@@ -3,4 +3,10 @@ part of 'home_bloc.dart';
 @immutable
 sealed class HomeState {}
 
-final class HomeInitial extends HomeState {}
+final class HomeLoading extends HomeState {}
+final class HomeError extends HomeState {}
+final class HomeLoaded extends HomeState {
+  final HomeModel home;
+
+  HomeLoaded(this.home);
+}
