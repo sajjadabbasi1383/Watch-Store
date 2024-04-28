1601715b685a44a666bc4814f039b9288ae8e9ec
SajjadAbbasi <sajjadabbas0083@gmail.com>
2024-04-28 16:27:39 +0330

    implement home bloc

--- lib/screens/home/bloc/home_bloc.dart
+++ lib/screens/home/bloc/home_bloc.dart
@@ -1,13 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:watch_store/data/model/home_model.dart';
+
+import '../../../data/repo/home_repo.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
+  final IHomeRepo homeRepo;
+  HomeBloc(this.homeRepo) : super(HomeLoading()) {
+    on<HomeEvent>((event, emit) async {
+      if(event is HomeInit){
+        try{
+          emit(HomeLoading());
+          final home=await homeRepo.getHome();
+          emit(HomeLoaded(home));
+        }catch(e){
+          emit(HomeError());
+        }
+      }
     });
   }
 }
